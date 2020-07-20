b1252d759aee35fb3eb336319a98713956b44061
Jie Luo <jieluo@berkeley.edu>
2020-07-19 22:07:33 -0700

    add all files from previous changes oops

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -8,12 +8,16 @@
         <g-link class="nav__link" to="/announcements/">Announcements</g-link>
         <g-link class="nav__link" to="/staff/">Course Staff</g-link>
         <g-link class="nav__link" to="/resources/">Resources</g-link>
-        <a class="nav__link" href="https://docs.google.com/document/d/1fm0MRPCziaeJ7b-c65uh2qUOurnNdhsP7ZCXcuPQwMk/edit">Syllabus</a>
+        <g-link class="nav__link" to="/policies/">Policies</g-link>
         <a class="nav__link" href="https://piazza.com/class/">Piazza</a>
       </nav>
     </header>
     <slot />
   <nav class="blank" />
+  <hr />
+  <footer class="footer">
+    <g-image src="~/assets/GitHub-Mark-32px.png" /><a class="nav__link" href="https://github.com/jie-luo/cloud-computing-decal">Website Repository</a>
+  </footer>
   </nav>
 </template>
 
@@ -28,7 +32,7 @@ query {
 <style>
 
 .layout {
-  max-width: 760px;
+  max-width: 900px;
   margin: 0 auto;
   padding-left: 20px;
   padding-right: 20px;
@@ -43,10 +47,19 @@ query {
 }
 
 .blank {
-  min-height: 200px;
+  min-height: 30px;
 }
 
 .nav__link {
   margin-left: 20px;
+}
+
+.footer {
+  display: flex;
+  align-items: center;
+  margin: 30px;
+}
+.footer a {
+  font-size: 18px;
 }
 </style>
