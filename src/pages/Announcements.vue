a70bc519d9f67bd1a7d2a6220c454dfd3712b140
Jie Luo <jieluo@berkeley.edu>
2020-07-19 22:07:14 -0700

    fix announcements page hr

--- src/pages/Announcements.vue
+++ src/pages/Announcements.vue
@@ -1,13 +1,13 @@
 <template>
   <Layout>
     <h1>Announcements</h1>
-    <nav v-for="edge in $page.announcements.edges" :key="edge.node.id">
+    <nav v-for="(edge, i) in $page.announcements.edges" :key="edge.node.id">
       <AnnouncementHolder 
         :title="edge.node.title" 
         :date="edge.node.date"
         :content="edge.node.content"
       />
-      <hr />
+      <span v-if="i != $page.announcements.edges.length - 1"><hr /></span>
     </nav>
   </Layout>
 </template>
